135d77a72bdc0bedca634058b90768d60c98aed1
Duvindu Dassanayake <ddassanayake@virtusa.com>
2019-07-26 18:08:32 +0530

    2nd commit

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts =[{'Author' : 'John', 'Title': 'book1', 'Content': 'Sci fi','Date_Posted': 'April 20 2018'}, { 'Author': 'Ryan', 'Title': 'book2', 'Content': 'Action', 'Date_Posted': 'April 25 2018'}]
+
 
 @app.route('/')
-def hello():
-    return render_template('home.html')
+def home():
+    return render_template('home.html', posts = posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title = 'About Page')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
