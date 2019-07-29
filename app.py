9ba45d4cc04dcd8ea9f26e0dfcc5e02c1c970bdf
Duvindu Dassanayake <ddassanayake@virtusa.com>
2019-07-29 20:03:07 +0530

    bootstrap navbar added

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-posts =[{'Author' : 'John', 'Title': 'book1', 'Content': 'Sci fi','Date_Posted': 'April 20 2018'}, { 'Author': 'Ryan', 'Title': 'book2', 'Content': 'Action', 'Date_Posted': 'April 25 2018'}]
+posts = [{'Author': 'John', 'Title': 'book1', 'Content': 'Sci fi', 'Date_Posted': 'April 20 2018'},
+         {'Author': 'Ryan', 'Title': 'book2', 'Content': 'Action', 'Date_Posted': 'April 25 2018'}]
 
 
 @app.route('/')
 def home():
-    return render_template('home.html', posts = posts)
+    return render_template('home.html', title="Home", posts=posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html',title = 'About Page')
+    return render_template('about.html', title='About Page')
 
 
 if __name__ == "__main__":
